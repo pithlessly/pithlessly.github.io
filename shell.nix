dc8a9feec744de90b331f902a2ebae80f0a6d30b
pithlessly <79002325+pithlessly@users.noreply.github.com>
2024-11-19 13:46:23 -0800

    shell.nix: include nodejs

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  cmark = pkgs.cmark;
   soupault = pkgs.soupault.overrideAttrs (final: prev: {
     nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.makeWrapper ];
     postInstall = (prev.postInstall or "") + ''
       wrapProgram $out/bin/soupault \
-        --prefix PATH : ${pkgs.lib.makeBinPath [ cmark ]}
+        --prefix PATH : ${pkgs.lib.makeBinPath [
+          pkgs.cmark
+          pkgs.nodejs
+        ]}
     '';
   });
 in
